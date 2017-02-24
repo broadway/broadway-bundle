5711185bf8b8531802d2cbc24bfa10f2c9afbd6a
Sander van Thillo <othillo@othillo.nl>
2017-02-24 12:35:10 +0100

    functionally test bundle

---
+++ test/AppKernel.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the broadway/broadway package.
+ *
+ * (c) Qandidate.com <opensource@qandidate.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Broadway\Bundle\BroadwayBundle;
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+class AppKernel extends Kernel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        return [
+            new BroadwayBundle(),
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        // TODO: Implement registerContainerConfiguration() method.
+    }
+
+}
---
+++ test/BroadwayBundleTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the broadway/broadway package.
+ *
+ * (c) Qandidate.com <opensource@qandidate.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Broadway\Bundle\BroadwayBundle;
+
+use Symfony\Bundle\FrameworkBundle\Tests\Functional\WebTestCase;
+
+/**
+ * @group functional
+ */
+class BroadwayBundleTest extends WebTestCase
+{
+    /**
+     * @test
+     */
+    public function it_does_not_throw_when_booting_kernel()
+    {
+        static::bootKernel();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected static function createKernel(array $options = [])
+    {
+        return new AppKernel('test', true);
+    }
+}
