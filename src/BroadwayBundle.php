48eb0e897970d3d6d89d9ceaee0d89b34c7493fc
othillo <othillo@othillo.nl>
2020-03-06 14:39:22 +0100

    fixup! fixup! added license

--- src/BroadwayBundle.php
+++ src/BroadwayBundle.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the broadway/broadway package.
+ * This file is part of the broadway/broadway-bundle package.
  *
- * (c) Qandidate.com <opensource@qandidate.com>
+ * (c) 2020 Broadway project
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
